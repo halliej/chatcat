ab0838d967e08813ca7d6228c9080967b95cc6ac
Hal DeLage <hal.delage@teamaol.com>
2017-02-27 05:52:35 -0500

    Started adding socket.io

--- app/index.js
+++ app/index.js
@@ -1,9 +1,26 @@
 'use strict';
+/* eslint-disable global-require */
+/* eslint-disable no-param-reassign */
 
 require('./auth')();
 
-/* eslint-disable global-require */
+const ioServer = app => {
+  app.locals.chatrooms = [];
+  const server = require('http').Server(app);
+  const io = require('socket.io')(server);
+
+  io.use((socket, next) => {
+    require('./session')(socket.request, {}, next);
+  });
+
+  require('./socket')(io, app);
+
+  return server;
+};
+
+
 module.exports = {
   router: require('./routes')(),
-  session: require('./session')
+  session: require('./session'),
+  ioServer
 };
--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.use(passport.session());
 
 app.use('/', chatCat.router);
 
-app.listen(app.get('port'), () => {
+chatCat.ioServer(app).listen(app.get('port'), () => {
   console.log('ChatCat listening on port', app.get('port'));
 });
